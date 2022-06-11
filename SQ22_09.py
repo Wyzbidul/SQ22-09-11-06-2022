95496d222b9e218b94aa91852b60f0fa20802827
MC <62519464+Wyzbidul@users.noreply.github.com>
2022-06-11 18:02:36 +0900

    Update SQ22_09.py

--- SQ22_09.py
+++ SQ22_09.py
@@ -11,12 +11,10 @@ from pandas import *
 from pandas.plotting import parallel_coordinates, scatter_matrix
 
 """Data Download"""
-
 rcParams["figure.autolayout"] = True
 iris = read_csv('./iris.data', sep=",", header=None, names= ["Sepal length", "Sepal width", "Petal length", "Petal width", "Species"])
 
 """Scatter Plot Matrix"""
-
 colors = array(50*['r']+50*['g']+50*['b'])
 scatter_matrix(iris,c=colors,figsize=(7,7))
 show()
